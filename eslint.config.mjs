2284d5c86786067c9d31156c7dcd2a183c9ffd7a
Driplase <258vr.g.subspeed@gmail.com>
2026-03-12 20:41:31 +0900

    chore: comma-dangle: "off", update packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,9 @@
 // @ts-check
 import withNuxt from './.nuxt/eslint.config.mjs'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default withNuxt(
-  // Your custom configs here
+  stylistic.configs.customize({
+    "comma-dangle": "off"
+  })  
 )
