5f6e585ba553ea6052e4f8b88858e1d832f7ca1a
driplase <d@j0.si>
2026-09-21 15:46:25 +0900

    feat: about page has more infos chore: readme updated

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -7,17 +7,42 @@ const { data: rules } = await useAsyncData(() => queryCollection('content').path
 </script>
 
 <template>
-  <div>
-    <UPageSection
-title="サーバールール" :ui="{
-      container: 'text-black dark:text-white lg:pt-24'
-    }">
-      <template #description>
+  <UPage>
+    <UPageBody>
+      <UPageSection
+        title="ScratchJPマインクラフトサーバーについて" 
+        :ui="{
+          container: 'text-black dark:text-white gap-2 sm:gap-4 py-4 sm:py-6 lg:py-8',
+          description: 'text-default',
+        }"
+      >
+        <template #description>
+          <p>
+            ScratchJPマインクラフトサーバーは、2023年3月30日に始動したMinecraftサーバーであり、<NuxtLink
+              to="https://scratchjp.org"
+              class="text-info hover:underline"
+              target="_blank"
+            >Scratch JP</NuxtLink>によって運営されています。
+          </p>
+          <p>
+            シーズン制で運用されており、今のSeason IIは2026年1月3日にスタートしました。
+          </p>
+          <br />
+          <p>バージョン: Java Edition 26.2</p>
+        </template>
+      </UPageSection>
+
+      <UPageSection
+        title="サーバールール" 
+        :ui="{
+          container: 'text-black dark:text-white gap-2 sm:gap-4 py-4 sm:py-6 lg:py-8',
+        }"
+      >
         <UContainer class="text-sm sm:text-base">
           <ContentRenderer v-if="rules" :value="rules" />
           <p v-else>サーバールールの読み込みに失敗しました；；</p>
         </UContainer>
-      </template>
-    </UPageSection>
-  </div>
+      </UPageSection>
+    </UPageBody>
+  </UPage>
 </template>
